21074ee1029291a6b9e113b1018f47a62c89f014
Peter Klimai <pklimai@gmail.com>
2023-07-14 22:09:46 +0300

    Fix all package versions

--- server/main.py
+++ server/main.py
@@ -6,11 +6,11 @@ from dash.dependencies import Input, Output, State
 import dash_extensions as de
 import dash_bootstrap_components as dbc
 
-# import dash_core_components as dcc
-from dash import dcc
+import dash_core_components as dcc
+# from dash import dcc
 
-# import dash_html_components as html
-from dash import html
+import dash_html_components as html
+# from dash import html
 
 import nica_dash_components
 
